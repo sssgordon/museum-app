48de81c9433be7fbde12805612cd8025356c04d3
sssgordon <gso7@uwo.ca>
2019-10-11 13:22:08 +0200

    Create elements with data form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,3 +1,18 @@
-const submitComment = function() {
-    console.log("Hello!")
+function submitComment() {
+    const inputField = document.getElementById("name")
+    const name = inputField.value
+    const textarea = document.getElementById("msg")
+    const msg = textarea.value
+
+    const comment = document.createElement("section")
+    const h3 = document.createElement("h3")
+    const p = document.createElement("p")
+
+    h3.innerHTML = `${name} said:`
+    p.innerHTML = msg
+    comment.classList.add("comment")
+    comment.appendChild(h3)
+    comment.appendChild(p)
+
+    console.log(comment)
 }
